92106efda8d75f20cd5fd2a2ce28a39b6c8a66e3
ebanjo-2 <eike.thed@gmail.com>
2020-04-26 10:14:51 +0200

    loading an xml file and printing out some xml elements from that file

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <engine/engine.h>
 
+#include <engine/file_loading/xml/xml_file.h>
+
+using namespace undicht;
+
 int main() {
 
-    undicht::Engine::initialize();
+    Engine::initialize();
 
-    undicht::window::Window* window = undicht::Engine::s_main_window;
+    {
 
-    while(!window->shouldClose()) {
+        window::Window* window = Engine::s_main_window;
 
-        window->update();
+        // testing xml file loading
+        XmlFile file("res/SupertuxTex.dae");
+
+
+        std::vector<const XmlElement*> elements = file.getAllElements({"COLLADA", "library_effects", "effect", "profile_COMMON", "newparam sid=\"Tux_jpg-surface\""});
+
+        for(const XmlElement* elem : elements) {
+
+            elem->printRecursive();
+        }
+
+        while(!window->shouldClose()) {
+
+            window->update();
+
+        }
 
     }
 
-    undicht::Engine::terminate();
+    Engine::terminate();
 
 }
